f5963f07d7aca6a4c47bdef50616900553bd3255
Stefan Oehme <st.oehme@gmail.com>
2014-12-08 22:02:31 +0100

    simplified by using gradle system prop convention

--- .travis/releaseOnTag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-if [ -n "${TRAVIS_TAG}" ]; then
-  ./gradlew release "-PbintrayApiKey=${BINTRAY_API_KEY}" "-Psigning.password=${SIGNING_PASSWORD}" "-PreleaseVersion=${TRAVIS_TAG}"
-fi
