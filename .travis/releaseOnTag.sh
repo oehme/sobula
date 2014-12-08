4581be088dd1cc3fb9baf9f5efedca37ae06efcd
Stefan Oehme <st.oehme@gmail.com>
2014-12-08 15:25:09 +0100

    fixed typo

--- .travis/releaseOnTag.sh
+++ .travis/releaseOnTag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ -n "${TRAVIS_TAG}" ] && ["${TRAVIS_PULL_REQUEST}" = "false"]; then
-  ./gradlew release"-PbintrayApiKey=${BINTRAY_API_KEY}" "-Psigning.password=${SIGNING_PASSWORD}" "-PreleaseVersion=${TRAVIS_TAG}"
+  ./gradlew release "-PbintrayApiKey=${BINTRAY_API_KEY}" "-Psigning.password=${SIGNING_PASSWORD}" "-PreleaseVersion=${TRAVIS_TAG}"
 fi
