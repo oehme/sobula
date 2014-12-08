86824a75e392b62641797cc2461eb40d2260aa46
Stefan Oehme <st.oehme@gmail.com>
2014-12-08 10:52:04 +0100

    escape all gradle parameters

--- .travis/releaseOnTag.sh
+++ .travis/releaseOnTag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
-  ./gradlew release -PbintrayUsername=$BINTRAY_USERNAME -PbintrayApiKey=$BINTRAY_API_KEY -Psigning.password=$SIGNING_PASSWORD -PsonatypeUsername=$SONATYPE_USERNAME -PsonatypePassword=$SONATYPE_PASSWORD -PreleaseVersion=$TRAVIS_TAG
+  ./gradlew release "-PbintrayUsername=${BINTRAY_USERNAME}" "-PbintrayApiKey=${BINTRAY_API_KEY}" "-Psigning.password=${SIGNING_PASSWORD}" "-PsonatypeUsername=${SONATYPE_USERNAME}" "-PsonatypePassword=${SONATYPE_PASSWORD}" "-PreleaseVersion=${TRAVIS_TAG}"
 fi
