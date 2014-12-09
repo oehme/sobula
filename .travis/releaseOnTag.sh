a729b17b87a1d7f7e9d241a9a8cbc450d748c465
Stefan Oehme <st.oehme@gmail.com>
2014-12-09 11:24:24 +0100

    use gradle env properties

--- .travis/releaseOnTag.sh
+++ .travis/releaseOnTag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
-  ./gradlew release "-PbintrayApiKey=${BINTRAY_API_KEY}" "-Psigning.password=${SIGNING_PASSWORD}" "-PsonatypePassword=${SONATYPE_PASSWORD}" "-PreleaseVersion=${TRAVIS_TAG}"
+  ./gradlew release "-PreleaseVersion=${TRAVIS_TAG}"
 fi
